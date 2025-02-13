1b55340a11bc42e6846597632e772af42973031b
Chakrasai <chakrasaiaku@gmail.com>
2025-02-13 12:02:11 +0530

    Update CORS origin to production URL in index.js

--- bcd/index.js
+++ bcd/index.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 
 const app = express();
 
-app.use(cors({ credentials: true, origin: 'http://localhost:3000' })); 
+app.use(cors({ credentials: true, origin: 'https://spam-frontend.onrender.com' })); 
 app.use(express.json());
 
 const flaskapi = 'https://spam-deployment.onrender.com/detectspam';
